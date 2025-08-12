8895f20077f9d27399fa2496386f7d1826d06474
gurucoding170-glitch <gurucoding170@gmail.com>
2025-08-12 08:20:27 +0000

    Added Capacitor setup and Android platform

---
+++ android/app/src/main/java/com/irsa/marketplaza/MainActivity.java
@@ -0,0 +1,5 @@
+package com.irsa.marketplaza;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.irsa.marketplaza',
+  appName: 'IrsaMarketPlaza',
+  webDir: 'dist'
+};
+
+export default config;
